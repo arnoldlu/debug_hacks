76c8e190db77a54471bbbfcfb1fe03e05a119174
Arnold Lu <arnoldlu@qq.com>
2018-10-17 07:14:39 +0800

    Add gdb_script

---
+++ gdb_script/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int total = 0;
+
+int square(int i)
+{
+    int result=0;
+
+    result = i*i;
+
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    for(i=0; i<10; i++)
+    {
+        total += square(i);
+    }
+    return 0;
+}
